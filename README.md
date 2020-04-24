47310f8ae4f496e396821ec4c532991270afc9cd
Frieder Bluemle <frieder.bluemle@walmartlabs.com>
2020-04-24 12:14:34 -0700

    tmp switch to fbtmp

--- README.md
+++ README.md
@@ -27,8 +27,8 @@ The [Docusaurus documentation][5] provide many useful guides, for example:
 - Creating [Custom Pages][6]
 - How [Navigation and Sidebars][7] work
 
-[1]: https://github.com/electrode-io/electrode-native-website/workflows/ci/badge.svg
-[2]: https://github.com/electrode-io/electrode-native-website/actions
+[1]: https://github.com/fbtmp/electrode-native-website/workflows/ci/badge.svg
+[2]: https://github.com/fbtmp/electrode-native-website/actions
 [3]: https://www.electrodenative.dev/
 [4]: https://docusaurus.io/
 [5]: https://docusaurus.io/docs/en/installation
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,17 +16,17 @@ const users = [
 const siteConfig = {
   title: 'Electrode Native', // Title for your website.
   tagline: '',
-  url: 'https://www.electrodenative.dev', // Your website URL
+  url: 'https://fbtmp.electrodenative.dev', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
-  cname: 'www.electrodenative.dev',
+  cname: 'fbtmp.electrodenative.dev',
 
   // Used for publishing and more
   projectName: 'electrode-native-website',
-  organizationName: 'electrode-io',
+  organizationName: 'fbtmp',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
